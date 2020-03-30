43d3da9747158f417d20e0b6416558970571a8fc
Kat Maldon <katharinamaldon@gmail.com>
2020-03-30 12:44:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,11 +4,10 @@ require 'directors_database'
 require 'pp'
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
   pp directors_database
   nil
 end
 
 def print_first_directors_movie_titles
-  directors_database.
+  directors_database.each
 end
